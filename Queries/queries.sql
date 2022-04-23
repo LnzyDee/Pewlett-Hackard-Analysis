3499fbe56ee01449e2a8095dbf94757e5ae5b0c3
unknown <lindsay.dove@gmail.com>
2022-04-22 23:22:28 -0500

    challenge upload

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -120,3 +120,70 @@ LEFT JOIN dept_emp AS de
 ON (ri.emp_no = de.emp_no)
 WHERE de.dept_no IN ('d005', 'd007')
 ORDER BY de.dept_no;
+
+
+-- retrieve the emp_no, first_name, and last_name columns from the employees table
+select * FROM employees
+select * from titles
+-- retrieve the title, from_date, and to_date columns from the titles table
+-- create a new table using the INTO clause
+-- join both tables on the primary key
+-- filter the data on the birth_date column to retrieve the employees who were born
+-- between 1952 and 1955, then order by employee number
+-- export the retirement titles table from the previous step as retirement_titles.csv
+-- and save it to your data folder in the pewlett-hackard-analysis folder
+-- before exporting confirm look
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title, 
+	t.from_date, 
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+
+SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles
+
+SELECT COUNT(title),
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE de.to_date = ('9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility
